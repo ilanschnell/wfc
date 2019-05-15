d8d27ed3578343c83f965237df87455661e83ba2
Ilan Schnell <ilanschnell@gmail.com>
2019-05-15 12:03:32 -0500

    add readme

---
+++ README.md
@@ -0,0 +1,9 @@
+Word Frequency Count
+--------------------
+
+This small C program counts the frequency of all words in a text file.
+Usage (you might have to adjust `PYTHON_PREFIX` in `build.sh` before
+running this):
+
+    $ make
+    $ ./wfc <somefile>
--- build.sh
+++ build.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-PREFIX=/Users/ilan/a/envs/py37
 
 if [ `uname` == Darwin ]; then
+    PYTHON_PREFIX=/Users/ilan/a/envs/py37
+
     gcc -Wall -arch x86_64 \
-          -I${PREFIX}/include/python3.7m -c main.c -o main.o
+          -I${PYTHON_PREFIX}/include/python3.7m -c main.c -o main.o
     gcc -undefined dynamic_lookup -arch x86_64 \
-          -L${PREFIX}/lib -lpython3.7m main.o -o wfc
+          -L${PYTHON_PREFIX}/lib -lpython3.7m main.o -o wfc
     install_name_tool -change @rpath/libpython3.7m.dylib \
-                      ${PREFIX}/lib/libpython3.7m.dylib ./wfc
+                      ${PYTHON_PREFIX}/lib/libpython3.7m.dylib ./wfc
 fi
