5b76e285ece9176c2142fff40d87be8ffc2de3fc
Ilan Schnell <ilanschnell@gmail.com>
2020-08-13 23:25:44 -0500

    update Makefile for Python 3.9

--- build.sh
+++ build.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 if [ `uname` == Darwin ]; then
-    PYTHON_PREFIX=/Users/ilan/a/envs/py37
+    PYTHON_PREFIX=/Users/ilan/tmp/py39
 
     gcc -Wall -arch x86_64 \
-        -I${PYTHON_PREFIX}/include/python3.7m -c main.c -o main.o
+        -I${PYTHON_PREFIX}/include/python3.9 -c main.c -o main.o
     gcc -undefined dynamic_lookup -arch x86_64 \
-        -L${PYTHON_PREFIX}/lib -lpython3.7m main.o -o wfc
-    install_name_tool -change @rpath/libpython3.7m.dylib \
-                      ${PYTHON_PREFIX}/lib/libpython3.7m.dylib ./wfc
+        -L${PYTHON_PREFIX}/lib -lpython3.9 main.o -o wfc
+    #install_name_tool -change @rpath/libpython3.8.dylib \
+    #                  ${PYTHON_PREFIX}/lib/libpython3.8.dylib ./wfc
 fi
 if [ `uname` == Linux ]; then
     PYTHON_PREFIX=/usr
