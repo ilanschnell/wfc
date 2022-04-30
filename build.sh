c158d7cff1ebc7176ed3a324b3b4ae1fc1ebb763
Ilan Schnell <ilanschnell@gmail.com>
2022-04-29 21:06:12 -0600

    update for my Python 3.11

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 if [ `uname` == Darwin ]; then
-    PYTHON_PREFIX=/Users/ilan/tmp/py39
+    PYTHON_PREFIX=/Users/ilan/Mini3/envs/py311
 
     gcc -Wall -arch x86_64 \
-        -I${PYTHON_PREFIX}/include/python3.9 -c main.c -o main.o
+        -I${PYTHON_PREFIX}/include/python3.11 -c main.c -o main.o
     gcc -undefined dynamic_lookup -arch x86_64 \
-        -L${PYTHON_PREFIX}/lib -lpython3.9 main.o -o wfc
+        -L${PYTHON_PREFIX}/lib -lpython3.11 main.o -o wfc
     #install_name_tool -change @rpath/libpython3.8.dylib \
     #                  ${PYTHON_PREFIX}/lib/libpython3.8.dylib ./wfc
 fi
