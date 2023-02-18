6143141ce227b5dc8200af559ab9bb91ac3317a9
Ilan Schnell <ilanschnell@gmail.com>
2023-02-18 00:24:20 -0600

    update Darwin build

--- build.sh
+++ build.sh
@@ -7,8 +7,8 @@ if [ `uname` == Darwin ]; then
         -I${PYTHON_PREFIX}/include/python3.11 -c main.c -o main.o
     gcc -undefined dynamic_lookup -arch x86_64 \
         -L${PYTHON_PREFIX}/lib -lpython3.11 main.o -o wfc
-    #install_name_tool -change @rpath/libpython3.8.dylib \
-    #                  ${PYTHON_PREFIX}/lib/libpython3.8.dylib ./wfc
+    install_name_tool -change @rpath/libpython3.11.dylib \
+                      ${PYTHON_PREFIX}/lib/libpython3.11.dylib ./wfc
 fi
 if [ `uname` == Linux ]; then
     PYTHON_PREFIX=/usr
